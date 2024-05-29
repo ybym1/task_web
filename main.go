cfddf575a31caa06a5903e67c018854ebbd8edb9
ybym1 <>
2024-05-30 00:30:26 +0900

    ginセットアップ

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("hello, world")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
 }
