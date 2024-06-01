2ab11a955e87c507efab1679dba4bf381538a200
ybym1 <>
2024-06-01 18:14:50 +0900

    HTMLを表示する

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
+		})
+	})
+
+	r.LoadHTMLGlob("templates/*")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
 		})
 	})
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
